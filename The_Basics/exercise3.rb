c5d22d03845430f50e6cf7fba09f6ecd92c23adb
Alexander Wilson <apwilson97@gmail.com>
2020-06-12 22:05:51 +0900

    completed exercise 3 of The_Basics

---
+++ The_Basics/exercise3.rb
@@ -0,0 +1,8 @@
+movies = {:Jaws => 1975, :Troy => 2004, :Man_of_Steel => 2013, :Shrek => 2001, :Raiders_of_The_Lost_Ark => 1981}
+
+puts movies[:Jaws]
+puts movies[:Troy]
+puts movies[:Man_of_Steel]
+puts movies[:Shrek]
+puts movies[:Raiders_of_The_Lost_Ark]
+
