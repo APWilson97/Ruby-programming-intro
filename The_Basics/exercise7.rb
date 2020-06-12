662cebbc535dda733b219bbe45ff49cd0a514186
Alexander Wilson <apwilson97@gmail.com>
2020-06-12 22:33:12 +0900

    Completed exercise 7 of The_Basics

---
+++ The_Basics/exercise7.rb
@@ -0,0 +1,2 @@
+'The user was in the process of creating a hash but used a ) bracket instead of a curly } bracket at the end, which has led to the error in creating the hash. The ) was unexpected as hashes use {} brackets, hence the \'expecting\' part was expecting a } when the hash was being created.
+The \'from\' part is tellin the user specifically where the error has occurred by giving the file path, which line inside of the file and under what element.'
