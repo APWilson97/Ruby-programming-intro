f7bed20ed99c20c41762cdfaaefacdcd9377f442
Alexander Wilson <apwilson97@gmail.com>
2020-06-21 22:31:05 +0900

    Completed exercise 7 of Exercises

---
+++ Exercises/exercise7.rb
@@ -0,0 +1,2 @@
+'An Array is an ordered list of a collection of data where it uses indexes for each element within it.'
+'A Hash uses key-value pairs instead of a numbered ordered list used within an array. Each element is a key-value pair and is referenced using the key.'
